ccedc5bf4498dd647f23c3c9768dbbd906a53955
Ilya Goltsov <ilya_goltsov@list.ru>
2019-03-15 01:05:30 +0300

    Add litcom

---
+++ litcom/js/common.js
@@ -0,0 +1,38 @@
+// = app/libs/jquery/dist/jquery.min.js
+// = app/libs/bootstrap/js/bootstrap.bundle.min.js
+// = app/libs/bootstrap/js/modal.js
+
+$(function() {
+    // Custom JS
+
+    // overlay
+    $('#dismiss, .overlay').on('click', function () {
+            // hide sidebar
+            $('#sidebar').removeClass('active');
+            // hide overlay
+            $('.overlay').removeClass('active');
+    });
+
+        $('#sidebarCollapse').on('click', function () {
+            // open sidebar
+            $('#sidebar').addClass('active');
+            // fade in the overlay
+            $('.overlay').addClass('active');
+            $('.collapse.in').toggleClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+    // toTop button
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 400) {
+            $('#toTop').addClass("top-active");
+        } else {
+            $('#toTop').removeClass("top-active");
+        }
+    });
+    $('#toTop').click(function() {
+        $('body,html').animate({scrollTop:0}, 500);
+    });
+
+});
+
